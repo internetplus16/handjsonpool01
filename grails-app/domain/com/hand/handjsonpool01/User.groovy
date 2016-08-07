aedaf03a647fd0bb9aa40d88312002b401fb8fde
internetplus16@163.com <you@example.com>
2016-08-07 14:18:20 +0800

    by 11559

--- grails-app/domain/com/hand/handjsonpool01/User.groovy
+++ grails-app/domain/com/hand/handjsonpool01/User.groovy
@@ -1,8 +1,14 @@
 package com.hand.handjsonpool01
 
 class User {
-    
+    Integer id
+    String loginName
+    String password
+    String phone
+    Date creationDate
+    Date lastUpdateDate
 
     static constraints = {
+        
     }
 }
