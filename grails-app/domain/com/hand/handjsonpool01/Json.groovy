957c917716bf09a601cd9958ada184db2ce17d52
jack <jack@163.com>
2016-08-08 09:30:49 +0800

    8.8

--- grails-app/domain/com/hand/handjsonpool01/Json.groovy
+++ grails-app/domain/com/hand/handjsonpool01/Json.groovy
@@ -3,43 +3,26 @@ package com.hand.handjsonpool01
 class Json {
 
     static constraints = {
-		id(min:0)
+		id()
 		placeholderUrl(blank: false)
 		method(inList:["Get","Posts","Delete","Put"])
 		jsonData(blank: false)
-<<<<<<< HEAD
-		startDate(validator:{return (it>new Date())})
-		lastUpdateDate(validator: {return (it>startDate)} )
-=======
+		startDate()
+		lastUpdateDate()
 		//startDate(validator:{return (it>new Date())})
-		dateCreated()
-		lastUpdated()
-<<<<<<< HEAD
-//		project()
-=======
->>>>>>> f5bb3b01f95a2700416e67bcbe636c88378d5053
+		
+
 //		Project project
->>>>>>> d4313e3a1195bd444391ce0507d2ba890f0e42aa
     }
-
-	static belongsTo = [project:Project]
-
+	
 	Integer id
 	String placeholderUrl
 	String method
 	String jsonData
-<<<<<<< HEAD
-	Project project
-=======
 //	Project project
-<<<<<<< HEAD
 	Date createDate
 	Date lastUpdateDate
-=======
->>>>>>> d4313e3a1195bd444391ce0507d2ba890f0e42aa
-	Date dateCreated
-	Date lastUpdated
->>>>>>> f5bb3b01f95a2700416e67bcbe636c88378d5053
+
 	String toString()
 	{
 		return "${id},${placeholderUrl}"
--- grails-app/domain/com/hand/handjsonpool01/Project.groovy
+++ grails-app/domain/com/hand/handjsonpool01/Project.groovy
@@ -8,15 +8,11 @@ class Project {
     Date lastUpdateDate;
 
 //    static belongsTo = [user:User];
-    static hasMany = [json:Json]
+//    static hasMany = [json:Json]
     static constraints = {
         id();
         projectName(blank: false);
-<<<<<<< HEAD
-        description(blank: flase);
-=======
         description(blank: false);
->>>>>>> 10717
         creationDate();
         lastUpdateDate();
     }
