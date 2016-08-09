d5d59589758537946f02a16e96649dcb35167162
jack <jack@163.com>
2016-08-09 14:59:08 +0800

    only login can opreate

--- grails-app/conf/com/hand/handjsonpool01/AdminonlyFilters.groovy
+++ grails-app/conf/com/hand/handjsonpool01/AdminonlyFilters.groovy
@@ -2,14 +2,18 @@ package com.hand.handjsonpool01
 
 class AdminonlyFilters {
 
-    def filters = {
-        all(controller:'user', action:'edit|delete') {
-            before = {
-                /*if (session.user.loginName!="admin"){
-                    redirect(controller: "user",action: "index")
-                    return false;
-                }*/
+        def filters = {
+            all(controller: 'user|project|json', action: 'show|index|save|delete|edit|update') {
+                before = {
+                    if (!session?.user){
+                        flash.message="please login"
+                        redirect(controller: "user",action: "login")
+                        return false;
+                    }
+                }
+
             }
         }
-    }
+
+
 }
