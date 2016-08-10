763da64f1048143854acd29da290257fd416cf93
jack <jack@163.com>
2016-08-10 09:46:57 +0800

    9.46 8-10

--- grails-app/conf/com/hand/handjsonpool01/AdminonlyFilters.groovy
+++ grails-app/conf/com/hand/handjsonpool01/AdminonlyFilters.groovy
@@ -3,7 +3,7 @@ package com.hand.handjsonpool01
 class AdminonlyFilters {
 
         def filters = {
-            all(controller: 'user|project|json', action: 'show|index|save|delete|edit|update') {
+            all(controller: 'user|project|json', action: 'show|index|delete|edit|update') {
                 before = {
                     if (!session?.user){
                         flash.message="please login"
